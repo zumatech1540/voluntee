8fee736a87b1ede9f9ef633abb18ef3939a3e707
unknown <zumatech423@gmail.com>
2026-04-15 12:28:02 +0300

    fix requirements

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
 
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput
