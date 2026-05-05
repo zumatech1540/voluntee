fb94441a01d915939e389d2bcaa3a1722944a287
unknown <zumatech423@gmail.com>
2026-05-05 15:10:49 +0300

    Connect PostgreSQL database

--- voluntee/settings.py
+++ voluntee/settings.py
@@ -99,12 +99,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'voluntee.wsgi.application'
 
 
-# DATABASE
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
